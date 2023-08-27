bd5ceb2dfbfeca960491b6b58d3dac81a05463fe
akalanka <akalankapathirage22@gmail.com>
2023-08-27 14:16:43 +0530

    scroll and highlight: half done

--- extension/scripts/scroll-to.js
+++ extension/scripts/scroll-to.js
@@ -16,10 +16,33 @@ function scrollToMatch(text) {
   while (walker.nextNode()) {
     const node = walker.currentNode;
     if (node.textContent.match(regex)) {
-      node.parentElement.scrollIntoView({
-        behavior: "smooth",
-        block: "center",
-      });
+      console.log('node', node)
+      // Create a new span element to wrap the matched text
+      const span = document.createElement("span");
+      span.style.backgroundColor = "yellow";
+      const match = node.textContent.match(regex)[0]; // Get the matched text
+      // console.log('first', match)
+
+      const beforeMatch = node.textContent.substring(
+        0,
+        node.textContent.indexOf(match)
+      );
+      const afterMatch = node.textContent.substring(
+        node.textContent.indexOf(match) + match.length
+      );
+      const beforeText = document.createTextNode(beforeMatch);
+      const matchText = document.createTextNode(match);
+      const afterText = document.createTextNode(afterMatch);
+
+      span.appendChild(matchText);
+
+      // Replace the node content with the highlighted span
+      node.textContent = "";
+      node.appendChild(beforeText);
+      node.appendChild(span);
+      node.appendChild(afterText);
+
+      span.scrollIntoView({ behavior: "smooth", block: "center" });
       break;
     }
   }
