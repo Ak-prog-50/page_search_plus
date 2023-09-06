6a104de9634a6fd9b6e754f4c74855a91edb4b80
akalanka <akalankapathirage22@gmail.com>
2023-09-06 15:38:45 +0530

    delted backend folder

--- backend/src/services/sendEmail.ts
+++
@@ -1,7 +0,0 @@
-// call any third party api for sending e-mails. Can be changed any time later.
-
-function sendEmail(email: string, message: string) {
-  console.log(`Sending email to ${email} with message: ${message}`);
-}
-
-export { sendEmail };
